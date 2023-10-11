5fc5f6d17d288a6a51488e2612baece29e307d97
Hoangkhongvui <121917433+Hoangkhongvui@users.noreply.github.com>
2023-10-11 20:59:36 +0700

    add screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,240 +1,115 @@
 import React, { useState } from 'react';
 import {
-    View,
     Text,
-    StyleSheet,
-    SafeAreaView,
-    ScrollView,
-    TouchableOpacity,
+    View,
     Image,
-    TextInput,
-    ImageBackground
+    ImageBackground,
+    TouchableOpacity,
 } from 'react-native';
-import { Ionicons } from "@expo/vector-icons";
-import Detail from './Detail';
+import { Ionicons } from "@expo/vector-icons"
 
-function HomeScreen({ navigation }) {
-    const [text, setText] = useState('');
-
+function Detail({ navigation }) {
     return (
-        <SafeAreaView>
-            <ScrollView>
-                <View style={{ paddingVertical: 40, paddingHorizontal: 10 }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-                        <TouchableOpacity onPress={() => navigation.openDrawer()}>
-                            <Ionicons
-                                name='search'
-                                size={24}
-                                style={{ opacity: 0.5 }}>
-                            </Ionicons>
-                        </TouchableOpacity>
-                        <TextInput
-                            style={{ height: 40, borderColor: 'gray', borderWidth: 0, padding: 8, width: 100, marginRight: 250, marginTop: -8, }}
-                            value={text} // Giá trị hiện tại của TextInput
-                            placeholder="Search"
-                        />
-                        <TouchableOpacity style={{ backgroundColor: 'blue', borderRadius: 8 }}>
-                            <Ionicons
-                                name='menu'
-                                size={32}
-                                color='white'
-                            >
-                            </Ionicons>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ marginBottom: 30 }}>
-                        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                            <TouchableOpacity onPress={() => navigation.navigate('MenuScreen')}>
-                                <Text style={{ fontSize: 20, marginBottom: 15, marginTop: 10, fontWeight: 700 }}>Next to you</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{ flexDirection: "row", marginTop: 10 }}>
-                                <Text>On the map</Text>
-                                <Ionicons name="arrow-forward"
-                                    style={{ marginTop: 4, }}></Ionicons>
-                            </TouchableOpacity>
-                        </View>
-                        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ marginLeft: 10 }}>
-                            <TouchableOpacity
-                                style={{ marginRight: 10, marginLeft: 20, }}
-                                onPress={() => navigation.navigate('Detail')}
-                            >
-                                <View>
-                                    <ImageBackground
-                                        style={{
-                                            width: 200,
-                                            height: 200,
-                                            resizeMode: 'contain',
-                                            marginRight: 20,
-                                            borderRadius: 10,
-                                        }}
-                                        source={require('../assets/1P.jpg')}
-                                    >
-                                        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                                        <Text style={{marginTop: 2}}>Util 10.10</Text>
-                                        <Ionicons name="heart" size={24}></Ionicons>
-                                        </View>
-                                    </ImageBackground>
-                                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                        <Text style={{ fontSize: 14, width: 120 }}>Go for a walk and feed the dog</Text>
-                                        <Ionicons name="mail" size={24} style={{ marginRight: 30 }} color="blue"></Ionicons>
-                                    </View>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={{ marginRight: 10, marginLeft: 20, }}
-                                onPress={() => navigation.navigate('Category')}
-                            >
-                                <View>
-                                    <ImageBackground
-                                        style={{
-                                            width: 200,
-                                            height: 200,
-                                            resizeMode: 'contain',
-                                            marginRight: 20,
-                                            borderRadius: 10,
-                                        }}
-                                        source={require('../assets/1P.jpg')}
-                                    >
-                                        <Ionicons name="heart" size={32} style={{ marginLeft: 160 }}></Ionicons>
-                                    </ImageBackground>
-                                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                        <Text style={{ fontSize: 14, width: 120 }}>Go for a walk and feed the dog</Text>
-                                        <Ionicons name="mail" size={24} style={{ marginRight: 30 }} color="blue"></Ionicons>
-                                    </View>
-                                </View>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={{ marginRight: 10, marginLeft: 20, }}
-                                onPress={() => navigation.navigate('Category')}
-                            >
-                                <View>
-                                    <ImageBackground
-                                        style={{
-                                            width: 200,
-                                            height: 200,
-                                            resizeMode: 'contain',
-                                            marginRight: 20,
-                                            borderRadius: 10,
-                                        }}
-                                        source={require('../assets/1P.jpg')}
-                                    >
-                                        <Ionicons name="heart"></Ionicons>
-                                    </ImageBackground>
-                                    <Text>Hamburger</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </ScrollView>
-                    </View>
-                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                        <TouchableOpacity onPress={() => navigation.navigate('MenuScreen')}>
-                            <Text style={{ fontSize: 20, marginBottom: 15, marginTop: 10, fontWeight: 700 }}>Categories</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{ flexDirection: "row", marginTop: 10 }}>
-                            <Text>See all</Text>
-                            <Ionicons name="arrow-forward"
-                                style={{ marginTop: 4, }}></Ionicons>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <TouchableOpacity onPress={() => { }} style={{
-                            flexDirection: "row"
-
-                        }}>
-                            <View>
-                                <Image
-                                    resizeMode='contain'
-                                    source={require('../assets/1P.jpg')}
-                                    style={{
-                                        height: 80,
-                                        width: 80,
-                                    }}
-                                />
-                            </View>
-                            <View style={{ marginTop: 20, }}>
-                                <Text style={{ fontWeight: 700, fontSize: 18 }}>Moving and things</Text>
-                                <Text style={{fontSize: 12}}>24 tasks</Text>
-                            </View>
-                            <Ionicons name="arrow-forward" size={32} style={{ marginTop: 24, marginLeft: 160, opacity: 0.5, }}></Ionicons>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { }} style={{
-                            flexDirection: "row"
-
-                        }}>
-                            <View>
-                                <Image
-                                    resizeMode='contain'
-                                    source={require('../assets/1P.jpg')}
-                                    style={{
-                                        height: 80,
-                                        width: 80,
-                                    }}
-                                />
-                            </View>
-                            <View style={{ marginTop: 20, }}>
-                                <Text style={{ fontWeight: 700, fontSize: 18 }}>Help around the house</Text>
-                                <Text style={{ fontSize: 12}}>8 tasks</Text>
-                            </View>
-                            <Ionicons name="arrow-forward" size={32} style={{ marginTop: 24, marginLeft: 160, opacity: 0.5, }}></Ionicons>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', 
-                        position: 'absolute', bottom: 0, left: 0, right: 0,
-                        backgroundColor: 'white'}}>
-                        <TouchableOpacity onPress={() => { }}
-                            style={{marginTop: 12}}>
-                            <Ionicons name="list" size={32} color="blue"></Ionicons>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { }}
-                            style={{marginTop: 12}}>
-                            <Ionicons name="heart" size={32}></Ionicons>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { }}
-                            style={{backgroundColor: 'blue', borderRadius: 25}}>
-                            <Ionicons name="add" size={48}></Ionicons>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { }}
-                            style={{marginTop: 12}}>
-                            <Ionicons name="mail" size={32}></Ionicons>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { }}
-                            style={{marginTop: 12}}>
-                            <Ionicons name="person" size={32}></Ionicons>
-                        </TouchableOpacity>
-
-                    </View>
-
-                    {/* <View>
+        <View>
+            <View style={{
+                position: 'relative',
+                top: 0,
+                width: '100%',
+                height: 400,
+            }}>
+                <ImageBackground
+                    source={require('../assets/2p.jpg')}
+                    resizeMode='contain'
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                    }}
+                >
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                         <TouchableOpacity>
-                            <Text style={{ fontSize: 17, marginBottom: 15 }}>Most Popular</Text>
+                            <Ionicons name="arrow-down" size={32}></Ionicons>
                         </TouchableOpacity>
-                        <View>
-                            <TouchableOpacity>
-                                <Image
-                                    source={require('../assets/1P.jpg')}
-                                    style={styles.image}
-                                />
-                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <Text>Noodles</Text>
-                                    <Text>$200</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                    </View> */}
+                        <TouchableOpacity>
+                            <Ionicons name="link-outline" size={32}></Ionicons>
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
+            </View>
+            <View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: "space-between",
+                    margin: 20,
+                }}>
+                    <Text style={{
+                        fontWeight: 700, fontSize: 24, color: 'rgb(57,58,59)',
+                        width: 300,
+                    }}>Go for a walk on the beach and search for One Piece</Text>
+                    <Ionicons name="heart" size={36} color='red' style={{
+                        
+                    }}></Ionicons>
                 </View>
-            </ScrollView>
-        </SafeAreaView>
+                <View style={{
+                    marginHorizontal: 20, marginBottom: 20,
+                }}>
+                    <Text style={{
+                        fontSize: 18,
+                    }}>
+                        Looking closely at the floating clouds, I felt excitement arising in my heart. The journey to find the One Piece treasure of the Straw Hat Pirates begins here.</Text>
+                </View>
+                <View>
+                    <View style={{
+                        marginLeft: 20,
+                        flexDirection: 'row',
+                    }}>
+                        <Ionicons name="wallet" size={28}></Ionicons>
+                        <Text style={{
+                            fontWeight: 700, fontSize: 18,
+                            marginTop: 2, marginLeft: 8,
+                        }}>Reward 34$</Text>
+                    </View>
+                    <View style={{
+                        marginLeft: 20,
+                        flexDirection: 'row',
+                    }}>
+                        <Ionicons name="location" size={28}></Ionicons>
+                        <Text style={{
+                            fontWeight: 700, fontSize: 18,
+                            marginTop: 2, marginLeft: 8,
+}}>3 HERALD</Text>
+                        <Text style={{
+                            marginLeft: 12, marginTop: 6,
+                            color: 'rgb(72,72,72)'
+                        }}>
+                            400m from you
+                        </Text>
+                    </View>
+                </View>
+            </View>
+            <View style={{
+                width: "100%", height: "100%",
+                position: "relative", bottom: 0, right: 0, left: 0,
+                flexDirection: "row", justifyContent: "space-evenly",
+                marginTop: 30,
+            }}>
+                <Ionicons name="mail" size={48} color="rgb(87, 171, 250)"></Ionicons>
+                <TouchableOpacity style={{
+                    width: 200, height: 60,
+                    backgroundColor: 'rgb(87, 171, 250)',
+                    borderRadius: 20,
+                }}>
+                    <Text style={{
+                        color: "white",
+                        textAlign: 'center',
+                        fontSize: 18,
+                        marginTop: 14,
+                    }}>Respond</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+
+
+
     );
 }
 
-export default HomeScreen;
-
-const styles = StyleSheet.create({
-    section: {
-        marginBottom: 10,
-    },
-    image: {
-        width: '100%',
-        height: 250,
-        resizeMode: 'contain'
-    }
-});
+export default Detail;
